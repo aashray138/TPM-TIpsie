3c3b84edfab6d47c21c301c603f3893ef2a2492e
Aashray Shrestha <aashray.138@gmail.com>
2021-03-31 17:32:39 -0400

    First Commit

--- Tipsie/ViewController.swift
+++ Tipsie/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var tipSegments: UISegmentedControl!
+    @IBOutlet weak var total: UILabel!
+    
+    @IBOutlet weak var numberOfPeople: UITextField!
+    @IBOutlet weak var splitTotal: UILabel!
+    
+    var totalBill = 0.0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tipControl(_ sender: Any) {
 
+        let bill = Double(billAmount .text!) ?? 0
+        let tipPercent = [0.15 , 0.18 , 0.20]
+        let tip = bill * tipPercent[tipSegments.selectedSegmentIndex]
+        
+        let totalAmount = bill + tip
+        
+        tipAmount.text = String(format:"$%.2f", tip)
+        total.text = String(format:"$%.2f", totalAmount)
+        
+        totalBill = totalAmount
+        
+    }
+    
+    @IBAction func onSplit(_ sender: Any) {
+        
+        let number = Double(numberOfPeople .text!) ?? 1
+        let splitAmount = totalBill / number
+        
+        splitTotal.text = String(format:"$%.2f", splitAmount)
+    }
 }
 
